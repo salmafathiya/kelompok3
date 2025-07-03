62bf214b057227d79d5e5a2b2fbbb43573c21f30
salmafathiya <salma.rahma1011@gmail.com>
2025-07-03 16:54:25 +0700

    first commit

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -12,7 +12,7 @@ if(uri_string()!=""){
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>- Fathiya Store - <?php echo $hlm ?></title>
+  <title>- Kelontong Pakdhe No - <?php echo $hlm ?></title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
