4e5fa53f124f0c38e92ca635ce24168dcc030811
rulyhandika <rulyhandika2003@gmail.com>
2025-07-10 12:14:45 +0700

    ini baru lagi

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -9,7 +9,7 @@ class TransactionModel extends Model
     protected $table = 'transaction';
     protected $primaryKey = 'id';
     protected $allowedFields = [
-        'username', 'total_harga', 'alamat', 'ongkir', 'status', 'created_at', 'updated_at'
+        'username', 'total_harga', 'alamat', 'ongkir', 'status', 'bukti_pembayaran', 'created_at', 'updated_at'
     ];
 
     public function updateStatus ($id, $status)
